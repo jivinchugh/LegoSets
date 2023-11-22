383c810618a919dd26fd687648c364459763114f
Your Name <jchugh@myseneca.ca>
2023-11-21 19:11:13 -0500

    initial commit

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,99 +1,154 @@
-const setData = require("../data/setData"); 
-const themeData = require("../data/themeData"); 
 
-//create a variable called "sets", initialized to an empty array
-let sets = [];
+require('dotenv').config(); //enabless us to use process.env
+const Sequelize = require('sequelize');
+// set up sequelize to point to our postgres database
+let sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  port: 5432,
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false },
+  },
+});
 
-function initialize()
-{
-    return new Promise((resolve,reject)=>
-    {
-    sets=setData.map((set)=>
-        {
-            const temptheme=themeData.find((theme)=>theme.id === set.theme_id);
-            const tempset={...set};
-            if(temptheme)
-            {
-                tempset.theme=temptheme.name;
-            }
-            return tempset;
-        })
+// Define a "Project" model
+const Theme = sequelize.define(
+  'Theme',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true, // use "id" as a primary key
+      autoIncrement: true, // automatically increment the value
+    },
+    name: Sequelize.STRING,
+  },
+  {
+    createdAt: false, // disable createdAt
+    updatedAt: false, // disable updatedAt
+  }
+);
+
+
+const Set = sequelize.define(
+  'Set',
+  {
+    set_num: {
+      type: Sequelize.STRING,
+      primaryKey: true, // use "set_num" as a primary key
+    },
+    name: Sequelize.STRING,
+    year: Sequelize.INTEGER,
+    num_parts: Sequelize.INTEGER,
+    theme_id: Sequelize.INTEGER,
+    img_url: Sequelize.STRING,
+  },
+  {
+    createdAt: false, // disable createdAt
+    updatedAt: false, // disable updatedAt
+  }
+);
+
+//type of relationship
+Set.belongsTo(Theme, { foreignKey: 'theme_id' });
+
+sequelize.sync()
+  .then(() => { console.log("data synched") })
+  .catch(err => { console.log(err) })
+
+function initialize() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await sequelize.sync();
+      resolve();
+    }
+    catch (error) {
+      reject(error.message);
+    }
+  });
+};
+
+
+function getAllSets() {
+  return new Promise((resolve, reject) => {
+    Set.findAll({ include: [Theme] })
+      .then((sets) => { resolve(sets) })
+      .catch((err) => { reject(err.message) });
+  })
+};
+
+
+function getSetByNum(setNum) {
+  return new Promise((resolve, reject) => {
+    Set.findAll({ where: { set_num: setNum }, include: [Theme] })
+      .then((sets) => { resolve(sets[0]) })
+      .catch((err) => { reject("Unable to find requested set") })
+  });
+};
+
+
+
+
+function getSetsByTheme(theme) {
+  return new Promise((resolve, reject) => {
+    Set.findAll({ include: [Theme], where: { '$Theme.name$': { [Sequelize.Op.iLike]: `%${theme}%` } } })
+      .then((themenew) => { resolve(themenew); })
+      .catch((err) => { reject("Unable to find requested sets"); });
+  });
+};
+
+function addSet(setData) {
+  return new Promise((resolve, reject) => {
+    Set.create({
+      set_num: setData.set_num,
+      name: setData.name,
+      year: setData.year,
+      num_parts: setData.num_parts,
+      theme_id: setData.theme_id,
+      img_url: setData.img_url
+    })
+      .then(() => { resolve() })
+      .catch((err) => { reject(err.errors[0].message) });
+  });
+}
+
+function getAllThemes() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let allThemes = await Theme.findAll();
+      resolve(allThemes);
+    }
+    catch (error) { reject(error) }
+  });
+}
+
+function editSet(set_num, setData) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const s = await Set.findOne({ where: { set_num: set_num } });
+      if (s) {
+        await s.update(setData);
         resolve();
-    });
-};
-
-
-function getAllSets()
-{
-    //This function simply returns the complete "sets" array
-    return new Promise((resolve,reject)=>
-    {
-        if(sets)
-        {
-            resolve(sets);
-        }
-        else
-        {
-            reject("not found")
-        }
-    })
-};
-
-
-function getSetByNum(setNum)
-{
-    return new Promise((resolve,reject)=>
-    {
-    // for(const set of sets)
-    // {
-        
-    //     if(set.set_num===setNum)
-    //     {
-    //         return set; //in true condition
-    //     }
-    // }
-    //return null; //in false condition
-
-    const temp = sets.find(function(set)
-    {
-        return set.set_num===setNum;
-    })
-
-    if(temp)
-    {
-        resolve(temp);
+      } else { reject("Set not found") }
     }
-    else
-    {
-        reject("unable to find requested set")
+    catch (err) { reject(err.errors[0].message) }
+  });
+}
+
+function deleteSet(set_num) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const s = await Set.findOne({
+        where: { set_num: set_num }
+      });
+      if (s) {
+        await s.destroy();
+        resolve();
+      }
+      else{reject("Set not found")}
     }
-  
-    });
-};
-
-
-
-
-function getSetsByTheme(theme)
-{
-    return new Promise((resolve,reject)=>
-    {
-    const lc = theme.toLowerCase();
-    const tempx= sets.filter(s_theme=> s_theme.theme.toLowerCase().includes(lc));
-
-    if(tempx.length>0)
-    {
-        resolve(tempx);
+    catch(error){
+      reject (err.errors[0].message)
     }
-    else{
-        reject("unable to find requested setas")
-    }
-
-    });
-};
-
-initialize();
-//console.log(getAllSets());
-//console.log(getSetByNum("1000709804-1"))
-//console.log(getSetsByTheme("Pirates"))
-module.exports = { initialize, getAllSets, getSetByNum, getSetsByTheme }
+  });
+}
+module.exports = { initialize, getAllSets, getSetByNum, getSetsByTheme, getAllThemes, addSet, editSet, deleteSet }
--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 /******************************************************************************** 
-*  BTI325 – Assignment 02 
+*  BTI325 – Assignment 04 
 *  
 *  I declare that this assignment is my own work in accordance with Seneca's 
 *  Academic Integrity Policy: 
 *  
 *  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html 
   LINK ISSSSSSSSSS!! 
-  
+  https://weak-suit-duck.cyclic.app/
 *  
 *  Name: Jivin Chugh     Student ID: 156056210       Date: 3 November,2023 
 * 
 ********************************************************************************/
 const legoData = require("./modules/legoSets");
 const express = require("express");
-const path = require("path");
 const app = express();
 app.set('view engine', 'ejs');
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"))
-
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.render("home");
 });
@@ -28,75 +27,71 @@ app.get("/about", (req, res) => {
     res.render("about");
 });
 
-app.get("/lego/sets", (req, res) => {
-    if (req.query.theme) {
-        legoData.getSetsByTheme(req.query.theme)
-            .then((data) => res.status(200).render("sets",{sets:data}))
-            .catch((err) => res.status(404).render("404", {message: "I'm sorry, theme not found"})
-            );
-
+app.get("/lego/sets", async (req, res) => {
+    try {
+        if (req.query.theme) {
+            let sets = await legoData.getSetsByTheme(req.query.theme);
+            if (sets.length > 0) { res.render("sets", { sets: sets }); }
+            else { res.render('404', { message: "I'm Sorry, theme not found" }) }
+        }
+        else {
+            let sets = await legoData.getAllSets();
+            res.render("sets", { sets: sets });
+        }
     }
-    else{
-    legoData.getAllSets().then((data) => res.render("sets",{sets:data}))
-        .catch((err) =>
-            res.status(404).render("404")
-        );
-    }
+    catch (err) { res.render('404', { message: err }); }
 });
 
 app.get("/lego/sets/:id", (req, res) => {
     legoData
         .getSetByNum(req.params.id)
-        .then((data) => res.render("set", {set: data}))
-        .catch((err) =>
-            res.status(404).render("404", {message: "I'm sorry, sets not found"})
-        );
+        .then((data) => res.render("set", { set: data }))
+        .catch((err) => res.status(404).render("404", { message: "I'm sorry, sets not found" }));
 });
 
-/*app.get("/lego/sets/theme-demo", (req, res) => {
-    const theme = "tech"; 
-    legoData
-      .getSetsByTheme(theme)
-      .then(sets => res.json(sets))
-      .catch(error => {res.json(error)});
-  });*/
-
-app.use((req, res) => {
-    res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"})
-});
-
-/*
-// //legoData.initialize();
-app.get("/lego/sets", (req, res) => {
-    legoData.getAllSets().then((sets) => {
-        res.json(sets);
-    })
-});
-
-
-
-//method 1
-app.get("/lego/sets/num-demo", async (req, res) => {
-
+app.get("/lego/addSet", async (req, res) => {
     try {
-        const temp = "1000648795-1";
-        let foundtemp = await legoData.getSetByNum(temp);
-        res.send(foundtemp);
-    }
-    catch (err) {
-        res.json(err);//res.send("not found");
-    }
+        let themeData = await legoData.getAllThemes();
+        res.render("addSet", { theme: themeData });
+    } catch (err) { res.render('404', { message: err }); }
+});
+
+app.post("/lego/addSet", async (req, res) => {
+    try {
+        await legoData.addSet(req.body);
+        res.redirect("/lego/sets");
+    } catch (err) { res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` }); }
+});
+
+app.get("/lego/editSet/:num", async (req, res) => {
+    try {
+        const setData = await legoData.getSetByNum(req.params.num);
+        const themeData = await legoData.getAllThemes();
+        res.render('editSet', { themes: themeData, set: setData });
+    } catch (err) { res.status(404).render("404", { message: err }) }
+});
+
+app.post("/lego/editSet", async (req, res) => {
+    try {
+        await legoData.editSet(req.body.set_num, req.body);
+        res.redirect("/lego/sets");
+    } catch (err) { res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` }) }
 });
 
 
-//method 2
-app.get("/lego/sets/theme-demo", (req, res) => {
-    const theme = "Pirates";
-    legoData.getSetsByTheme(theme).then(sets =>
-        res.json(sets)).catch(err => {
-            res.json(err);
-        });
-});*/
+app.get("/lego/deleteSet/:num", async (req, res) => {
+    try {
+      await legoData.deleteSet(req.params.num);
+      res.redirect("/lego/sets");
+    } catch (error) {
+      res.render("500", { message: `I'm sorry, but we have encountered the following error: ${error}` });
+    }
+  });
+  
+
+app.use((req, res) => { res.status(404).render("404", { message: "I'm sorry, we're unable to find what you're looking for" }) });
+
+
 
 
 legoData.initialize().then(() => {
@@ -104,7 +99,3 @@ legoData.initialize().then(() => {
         console.log(`Server listening on : ${HTTP_PORT}`)
     })
 });
-
-// app.listen(HTTP_PORT,()=>{
-//     console.log(`server listening on : ${HTTP_PORT}`)
-// })
